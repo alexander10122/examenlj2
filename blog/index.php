0431e5231cbbb55b15ea6ba570341c21827cfd34
Alexander <alexander.12@live.nl>
2018-12-17 12:50:56 +0100

    basis bestanden ingevoegd
    ik heb 2 basis bestanden toegevoegd, 1 index.php bestand en een style.css, in de index komt straks de posts die worden laten zien

---
+++ blog/index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<link href="../CSS/blog.css" rel="stylesheet" type="text/css" />
+</head>
+<header>
+
+</header>
+<body>
+
+</body>
+<footer>
+
+</footer>
+</html>
