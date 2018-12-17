ce62cbe4eac9f66174f58f9edff4b65c77dfaeca
Alexander <alexander.12@live.nl>
2018-12-17 15:06:18 +0100

    If&else statement aangemaakt
    ik heb een if + else statement aangemaakt die nakijkt of hij wel een $pid mee heb gekregen waanneer de gebruiker op de delete knop heb gedrukt, als hij dus niets binnen heeft gekregen dan stuurt hij de gebruiker terug naar de index, anders wordt het proces doorgezet en wordt de post met de $pid verwijderd uit de database

--- blog/del_post.php
+++ blog/del_post.php
@@ -6,4 +6,14 @@ if(!isset($_SESSION['username'])) {
     header("Location: ../login_form.php");
     return;
 }
+if(!isset($_GET['pid'])) {
+    header("Location: index.php");
+}
+else {
+    $pid = $_GET['pid'];
+    $sql = "DELETE FROM posts WHERE id=$pid";
+    mysqli_query($con, $sql);
+    header("Location: index.php");
+}
+
 ?>
