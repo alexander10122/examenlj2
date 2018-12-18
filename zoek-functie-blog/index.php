75b5d84b97bc3ee9330b9072e922bc1d425380fd
Alexander <alexander.12@live.nl>
2018-12-18 09:36:03 +0100

    basis pagina ingevoegd
    ik heb een pasis pagina voor de zoekbar ingevoegd,

---
+++ zoek-functie-blog/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Stagair local spot">
+    <meta name="keywords" content="Technolab, Stagairs, Workshops">
+    <meta name="author" content="Ravi Breugom, Alexander Wallaard, Natascha van Baal">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Technolab - zoeken..</title>
+</head>
+<header>
+</header>
+<body>
+</body>
+    <footer id="copyright">&copy; Technolab Leiden</footer>
+</html>
